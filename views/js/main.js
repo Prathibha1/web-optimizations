f0f06e45a5ed030d740f83d57eb411a735933829
prathiba <prathibhasravani@gmail.com>
2016-11-24 09:25:00 +0530

    Update main.js

--- views/js/main.js
+++ views/js/main.js
@@ -448,13 +448,21 @@ var resizePizzas = function(size) {
   }
 
   // Iterates through pizza elements on the page and changes their widths
-  function changePizzaSizes(size) {
+ /* function changePizzaSizes(size) {
     for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
       var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
       var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
       document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
     }
+  }*/
+  function changePizzaSizes(size) {
+    var dx = determineDx(document.querySelector(".randomPizzaContainer"), size);
+    var newwidth = (document.querySelector(".randomPizzaContainer").offsetWidth + dx) + 'px';
+    var elements = document.querySelectorAll(".randomPizzaContainer");
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].style.width = newwidth;
   }
+}
 
   changePizzaSizes(size);
 
